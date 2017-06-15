c76ba40f9bf8fd5edce2c88973974c3389f4897a
yeskunall <yeskunall96@gmail.com>
2017-06-15 14:32:23 -0400

    :candy: nyts init

---
+++ README.md
@@ -0,0 +1,8 @@
+# nyts
+
+Node.js wrapper for the YTS API.
+
+<br />
+
+_... More info coming soon!_
+
---
+++ __test__/sample.test.js
@@ -0,0 +1,6 @@
+/* global test, expect */
+
+test('Generic test that should pass no matter what', () => {
+  const sum = 3;
+  expect(sum).toBe(3);
+});

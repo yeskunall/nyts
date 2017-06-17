c84c530bbba4025a4d44b9feccc9d9f5f364f675
Kunall Banerjee <yeskunall96@gmail.com>
2017-06-16 22:43:52 -0400

    Revert "✨ First major PR: build changes, code formatting changes, linting changes"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
-  extends: "airbnb-base",
-  plugins: ["import"],
-  rules: {
-    camelcase: [0, { properties: "never" }],
-    "no-console": 0
-  }
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ]
 };

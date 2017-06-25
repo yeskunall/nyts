4c4fdb8a43959756b62248d67e5c1ff582ef57c5
Arun Galva <arungalva@hotmail.com>
2017-06-24 20:16:11 -0400

    test: Added tests for param validation function
    Basic tests for verifyParams helper function
    
    #15

--- __test__/helpers.test.js
+++ __test__/helpers.test.js
@@ -1,6 +1,6 @@
 /* global describe, test, expect */
 
-const { isValidURL } = require("../lib/utils/helpers");
+const { isValidURL, verifyParams } = require("../lib/utils/helpers");
 
 describe("Test if valid URL(s)", () => {
   test("with no special characters", () => {
@@ -8,5 +8,36 @@ describe("Test if valid URL(s)", () => {
     expect(isValidURL("https://kimchi.surge.sh")).toBe(true);
     expect(isValidURL("http://")).toBe(false);
     expect(isValidURL("http://foo.bar?q=Spaces should be encoded")).toBe(false);
+  });
+});
+
+describe("Test if valid Pramas", () => {
+  test("correct inputs", () => {
+    const user_add_comment = {
+      user_key: "MY_FANCY_KEY",
+      movie_id: 12345,
+      application_key: 98765,
+      comment_text: "THIS A NICE MOVIE FAM",
+    };
+
+    expect(verifyParams("COMMENT_ADD", user_add_comment)).toBe(true);
+  });
+
+  test("missing a required input", () => {
+    const user_add_comment = {
+      user_key: "MY_FANCY_KEY",
+      application_key: 98765,
+      comment_text: "THIS A NICE MOVIE FAM",
+    };
+    expect(verifyParams("COMMENT_ADD", user_add_comment)).toBe(false);
+  });
+  test("wrong type of input", () => {
+    const user_add_comment = {
+      user_key: "MY_FANCY_KEY",
+      movie_id: "12345",
+      application_key: 98765,
+      comment_text: "THIS A NICE MOVIE FAM",
+    };
+    expect(verifyParams("COMMENT_ADD", user_add_comment)).toBe(false);
   });
 });

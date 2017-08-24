149d6f8945eb84e06cf9b2beca818c3899d41574
yeskunall <yeskunall96@gmail.com>
2017-08-24 12:03:17 -0400

    build(rimraf): some additions to the build process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,9 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import rimraf from 'rimraf';
 
-gulp.task('default', () =>
+gulp.task('clean', () => rimraf('./dist', () => {}));
+
+gulp.task('build', ['clean'], () =>
   gulp.src('./lib/**/*.js').pipe(babel()).pipe(gulp.dest('dist')),
 );

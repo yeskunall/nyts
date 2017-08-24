334ec440672323957475a1e8498963ae915e6b2c
yeskunall <yeskunall96@gmail.com>
2017-08-24 11:39:51 -0400

    build(gulp): fix build process (target Node 4.x.x)
    should fix #28

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,5 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 
 gulp.task('default', () =>
-  gulp.src('./lib/nyts.js')
-    .pipe(babel({
-      presets: ['env'],
-    }))
-    .pipe(gulp.dest('dist')),
+  gulp.src('./lib/**/*.js').pipe(babel()).pipe(gulp.dest('dist')),
 );

ff4c86ff823129f87bbb8258dff6336dfcdf8518
Kunall Banerjee <yeskunall96@gmail.com>
2017-11-08 00:31:21 +0000

    some house cleaning
    remove gulp. No longer needed.

--- __test__/sample.test.js
+++
@@ -1,6 +0,0 @@
-/* global test, expect */
-
-test('Generic test that should pass no matter what', () => {
-  const sum = 3;
-  expect(sum).toBe(3);
-});
--- gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import rimraf from 'rimraf';
-
-gulp.task('clean', () => rimraf('./dist', () => {}));
-
-gulp.task('build', ['clean'], () =>
-  gulp.src('./lib/**/*.js').pipe(babel()).pipe(gulp.dest('dist')),
-);

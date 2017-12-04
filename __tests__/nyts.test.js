9ccb6c69a74aba911189585915e411a3af856fce
Kunall Banerjee <yeskunall96@gmail.com>
2017-12-03 21:59:59 -0500

    fix failing tests
    A minor fix in the `request.js` module as well: the POST method was technically making GET requests. Note to self: this is what happens when you copy/paste code.
    
    Another note to self: I *really* need to write some better tests

--- __tests__/nyts.test.js
+++ __tests__/nyts.test.js
@@ -7,65 +7,65 @@ import Nyts from '../lib/nyts';
 
 const response = [
   {
-    id: 6860,
-    url: 'https://yts.ag/movie/lipstick-under-my-burkha-2016',
-    imdb_code: 'tt4807830',
-    title: 'Lipstick Under My Burkha',
-    title_english: 'Lipstick Under My Burkha',
-    title_long: 'Lipstick Under My Burkha (2016)',
-    slug: 'lipstick-under-my-burkha-2016',
-    year: 2016,
-    rating: 7,
-    runtime: 117,
-    genres: ['Drama'],
+    id: 6914,
+    url: 'https://yts.am/movie/how-the-grinch-stole-christmas-1966',
+    imdb_code: 'tt0060345',
+    title: 'How the Grinch Stole Christmas!',
+    title_english: 'How the Grinch Stole Christmas!',
+    title_long: 'How the Grinch Stole Christmas! (1966)',
+    slug: 'how-the-grinch-stole-christmas-1966',
+    year: 1966,
+    rating: 8.4,
+    runtime: 26,
+    genres: ['Animation', 'Comedy', 'Family', 'Fantasy', 'Musical'],
     summary:
-      'Set in the crowded by-lanes of small town India, Lipstick Under My Burkha chronicles the secret lives of four women in search of a little freedom. Though stifled and trapped in their worlds, these four women claim their desires through small acts of courage and stealthy rebellion.',
+      'Bitter and hateful, the Grinch is irritated at the thought of the nearby village having a happy time celebrating Christmas. So disguised as Santa Claus, with his dog made to look like a reindeer, he raids the village to steal all the Christmas things. The village is sure to have a sad Christmas this year.',
     description_full:
-      'Set in the crowded by-lanes of small town India, Lipstick Under My Burkha chronicles the secret lives of four women in search of a little freedom. Though stifled and trapped in their worlds, these four women claim their desires through small acts of courage and stealthy rebellion.',
+      'Bitter and hateful, the Grinch is irritated at the thought of the nearby village having a happy time celebrating Christmas. So disguised as Santa Claus, with his dog made to look like a reindeer, he raids the village to steal all the Christmas things. The village is sure to have a sad Christmas this year.',
     synopsis:
-      'Set in the crowded by-lanes of small town India, Lipstick Under My Burkha chronicles the secret lives of four women in search of a little freedom. Though stifled and trapped in their worlds, these four women claim their desires through small acts of courage and stealthy rebellion.',
-    yt_trailer_code: 'fxwXMpZ7AF8',
+      'Bitter and hateful, the Grinch is irritated at the thought of the nearby village having a happy time celebrating Christmas. So disguised as Santa Claus, with his dog made to look like a reindeer, he raids the village to steal all the Christmas things. The village is sure to have a sad Christmas this year.',
+    yt_trailer_code: 'XYRnwWmteac',
     language: 'English',
     mpa_rating: 'NR',
     background_image:
-      'https://yts.ag/assets/images/movies/lipstick_under_my_burkha_2016/background.jpg',
+      'https://yts.am/assets/images/movies/how_the_grinch_stole_christmas_1966/background.jpg',
     background_image_original:
-      'https://yts.ag/assets/images/movies/lipstick_under_my_burkha_2016/background.jpg',
+      'https://yts.am/assets/images/movies/how_the_grinch_stole_christmas_1966/background.jpg',
     small_cover_image:
-      'https://yts.ag/assets/images/movies/lipstick_under_my_burkha_2016/small-cover.jpg',
+      'https://yts.am/assets/images/movies/how_the_grinch_stole_christmas_1966/small-cover.jpg',
     medium_cover_image:
-      'https://yts.ag/assets/images/movies/lipstick_under_my_burkha_2016/medium-cover.jpg',
+      'https://yts.am/assets/images/movies/how_the_grinch_stole_christmas_1966/medium-cover.jpg',
     large_cover_image:
-      'https://yts.ag/assets/images/movies/lipstick_under_my_burkha_2016/large-cover.jpg',
+      'https://yts.am/assets/images/movies/how_the_grinch_stole_christmas_1966/large-cover.jpg',
     state: 'ok',
     torrents: [
       {
         url:
-          'https://yts.ag/torrent/download/74D7E62A2830E098A74D001651FBBCE3EC606211',
-        hash: '74D7E62A2830E098A74D001651FBBCE3EC606211',
+          'https://yts.am/torrent/download/801E6EE764B1C34FA4D4D150868154E74FF82937',
+        hash: '801E6EE764B1C34FA4D4D150868154E74FF82937',
         quality: '720p',
-        seeds: 898,
-        peers: 957,
-        size: '871.11 MB',
-        size_bytes: 913425039,
-        date_uploaded: '2017-11-08 07:29:39',
-        date_uploaded_unix: 1510144179,
+        seeds: 348,
+        peers: 63,
+        size: '183.61 MB',
+        size_bytes: 192529039,
+        date_uploaded: '2017-12-02 16:45:32',
+        date_uploaded_unix: 1512251132,
       },
       {
         url:
-          'https://yts.ag/torrent/download/3D845B04761F4A7F38A77EFB60021124C8FA4AD6',
-        hash: '3D845B04761F4A7F38A77EFB60021124C8FA4AD6',
+          'https://yts.am/torrent/download/E17C26153FAC8FA0BE33F3ACEC087B386B1F415B',
+        hash: 'E17C26153FAC8FA0BE33F3ACEC087B386B1F415B',
         quality: '1080p',
-        seeds: 656,
-        peers: 591,
-        size: '1.8 GB',
-        size_bytes: 1932735283,
-        date_uploaded: '2017-11-08 11:22:31',
-        date_uploaded_unix: 1510158151,
+        seeds: 662,
+        peers: 116,
+        size: '395.52 MB',
+        size_bytes: 414732780,
+        date_uploaded: '2017-12-02 17:38:47',
+        date_uploaded_unix: 1512254327,
       },
     ],
-    date_uploaded: '2017-11-08 07:29:39',
-    date_uploaded_unix: 1510144179,
+    date_uploaded: '2017-12-02 16:45:32',
+    date_uploaded_unix: 1512251132,
   },
 ];
 
@@ -76,7 +76,8 @@ test.before(() => {
 });
 
 test('this is a very useless test', async t => {
-  // better tests coming up!
+  // OK seriously, I really need to
+  // write some better tests 🙄
   const n = new Nyts(null, null, false);
   t.deepEqual(await n.getMovies(1).then(({ movies }) => movies), response);
 });
